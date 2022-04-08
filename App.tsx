9172e4a604de8f0e6bc313213999e633e27dc3af
Guillermo Rivas <grivas@pop-os.localdomain>
2022-04-08 11:30:29 -0500

    clase de abril 8

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 // everything is done through components
@@ -6,14 +7,25 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 // props 
 // la manera de agregar atributos parametrizables al componente 
 const Gatito = (props:any) => {
+  
+  // props se reciben del exterior
+  // estados son valores internos de los que cada instancia tiene una copia
+  // se modifican internamente
+
+  // definición de un estado 
+  const[cuenta, setCuenta] = useState(0);
+  const[comio, setComio] = useState(false);
+  const[nombre, setNombre] = useState("Garfiol");
+
 
   // los componentes regresan algún tipo de view
   return(<View>
-      <Text>ESTO ES UN GATITO Y SU NOMBRE ES: {props.nombre}</Text>
+      <Text>ESTO ES UN GATITO Y SU NOMBRE ES: {props.nombre} CUENTA: {cuenta}</Text>
       <Button
         title="EL GATITO"
         onPress={() => {
-          alert("UN MIAU " + props.mensaje);
+          alert("UN MIAU " + props.mensaje + " CUENTA: " + cuenta);
+          setCuenta(cuenta + 1);
         }}
        />
     </View>);
@@ -26,7 +38,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>HEY EVERYONE! </Text>
-      <Gatito />
+      <Gatito nombre="Misifus" mensaje="MIAU" />
+      <Gatito nombre="Fluffy" mensaje="MIAU2" />
+      <Gatito nombre="Gatito" mensaje="MIAU3" />
+      <Gatito nombre="Garfiol" mensaje="MIAU4" />
       <StatusBar style="auto" />
     </View>
   );
